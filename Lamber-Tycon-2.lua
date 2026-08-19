e4217a4075ddddf8b06919dbad2a0ba3296ae3ec
Fureyme <133177863+Fureyme@users.noreply.github.com>
2026-08-19 22:15:17 +0300

    Update Lamber-Tycon-2.lua

--- Lamber-Tycon-2.lua
+++ Lamber-Tycon-2.lua
@@ -144,17 +144,17 @@ if characterTool then
 			for toolName, indexTool in pairs(inventory) do --получаем index топора в Backpack
 				if nameAxes == nil then
 					hitPoints = getHitPoint(toolName, treeType)
-					tool = localPlayer.Backpack[indexTool]
+					tool = localPlayer.Backpack:GetChildren()[indexTool]
 					break
 				end
 				for _, nameAxe in pairs(nameAxes) do
 					if nameAxe == toolName then
 						hitPoints = getHitPoint(nameAxe, treeType)
 						print(type(indexTool), indexTool)
-						tool = localPlayer.Backpack[indexTool]
+						tool = localPlayer.Backpack:GetChildren()[indexTool]
 					else 
 						hitPoints = getHitPoint(toolName, treeType)
-						tool = localPlayer.Backpack[indexTool]
+						tool = localPlayer.Backpack:GetChildren()[indexTool]
 						break
 					end
 				end
